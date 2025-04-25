c561b653e98b1a350e043161c4645e48ad404ed9
Nithin Krishnaa <inithin683@gmail.com>
2025-04-25 21:24:10 +0530

    basic flake structure

---
+++ flake.nix
@@ -0,0 +1,7 @@
+{
+  description = "My Bastion modules flake";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  outputs = { self, nixpkgs, }: {
+    nixosModules.bastion = import ./modules;
+  };
+}
